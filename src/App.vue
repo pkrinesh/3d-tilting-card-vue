1af99b10a37673b5d36f2efab9b33b2b03b74528
Krinesh <pkrinesh95@gmail.com>
2023-08-17 16:24:00 +0530

    feat: follow the mouse 3d-card animation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,45 @@
 <script setup lang="ts">
-/* no-prettier */
+import { ref, watchEffect } from 'vue';
+
+const cardRef = ref<HTMLDivElement | null>(null);
+
+watchEffect(() => {
+  if (!cardRef.value) return;
+
+  const handleMouseMove = (e: MouseEvent) => {
+    rotateElement(e, cardRef);
+  };
+
+  document.addEventListener('mousemove', handleMouseMove);
+
+  return () => document.removeEventListener('mousemove', handleMouseMove);
+});
+
+function rotateElement(event: MouseEvent, element: typeof cardRef) {
+  if (!element.value) return;
+
+  // get mouse position
+  const x = event.clientX;
+  const y = event.clientY;
+
+  // find the middle
+  const middleX = window.innerWidth / 2;
+  const middleY = window.innerHeight / 2;
+
+  // get offset from middle as a percentage
+  // and tone it down a little
+  const offsetX = ((x - middleX) / middleX) * 45;
+  const offsetY = ((y - middleY) / middleY) * 45;
+
+  // set rotation
+  element.value?.style.setProperty('--rotateX', offsetX + 'deg');
+  element.value?.style.setProperty('--rotateY', -1 * offsetY + 'deg');
+}
 </script>
 
 <template>
   <div class="container">
-    <div class="card language-css" tabindex="0"></div>
+    <div ref="cardRef" class="card language-css" tabindex="0"></div>
   </div>
 </template>
 
