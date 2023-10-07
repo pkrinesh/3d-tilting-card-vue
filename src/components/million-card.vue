f2ddf99436fee02f94cc5bf70798c9d42d1c3d24
Krinesh <pkrinesh95@gmail.com>
2023-10-08 00:04:32 +0530

    fix: million card

--- src/components/million-card.vue
+++ src/components/million-card.vue
@@ -4,16 +4,16 @@ import { computed, reactive, ref } from 'vue'
 
 const cardRef = ref<HTMLDivElement | null>(null)
 const mouse = reactive(useMouseInElement(cardRef))
-const MAX_ROTATION = 10
+const MAX_ROTATION = 12
 
 const cardTransform = computed(() => {
-  const offsetX = (mouse.elementX / mouse.elementWidth) * MAX_ROTATION - MAX_ROTATION / 2
-  const offsetY = (mouse.elementY / mouse.elementHeight) * MAX_ROTATION - MAX_ROTATION / 2
+  const offsetX = (mouse.elementY / mouse.elementHeight) * MAX_ROTATION - MAX_ROTATION / 2
+  const offsetY = (mouse.elementX / mouse.elementWidth) * MAX_ROTATION - MAX_ROTATION / 2
 
   const rX = -1 * offsetX + 'deg'
   const rY = 1 * offsetY + 'deg'
 
-  return mouse.isOutside ? '' : `perspective(5000px) rotateY(${rX}) rotateX(${rY})`
+  return mouse.isOutside ? '' : `perspective(${mouse.elementWidth}px) rotateX(${rX}) rotateY(${rY})`
 })
 </script>
 
@@ -22,7 +22,7 @@ const cardTransform = computed(() => {
     <div
       ref="cardRef"
       tabindex="0"
-      class="card flex justify-center items-center"
+      class="card flex justify-center items-center shadow-lg bg-gradient-to-b from-zinc-700 to-zinc-800"
       :style="{
         // transform: cardTransform,
         // transition: 'transform 0.25s ease-out',
@@ -38,8 +38,9 @@ const cardTransform = computed(() => {
 .card {
   width: 400px;
   height: 200px;
-  color: gray;
-  background: hsl(222, 45%, 7%);
+  color: white;
+  /* background: #641ae6; */
+  /* background: #434343; */
   padding: 2rem;
   border-radius: 1rem;
 
@@ -48,35 +49,5 @@ const cardTransform = computed(() => {
   transition: transform 0.2s ease-out;
   transform-style: preserve-3d;
   transform: v-bind(cardTransform);
-}
-
-.card::before,
-.card::after {
-  content: '';
-  position: absolute;
-  border-radius: inherit;
-}
-
-/* shadow */
-.card::before {
-  inset: 0.75rem;
-  border-radius: inherit;
-  background: black;
-  z-index: -1;
-  transform: translateZ(-50px);
-  filter: blur(15px);
-  opacity: 0.5;
-}
-
-/* gradient thingy */
-.card::after {
-  z-index: -2;
-  inset: -1.2rem;
-
-  background: #aa076b;
-  background: -webkit-linear-gradient(to top, #61045f, #aa076b);
-  background: linear-gradient(to top, #61045f, #aa076b);
-
-  transform: translateZ(-50px);
 }
 </style>
